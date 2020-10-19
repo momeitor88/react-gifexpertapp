f54204ae43d8b6b1e44b610acaf69bdd144ac0e1
Carles Gertrudis <geerles@gmail.com>
2020-10-19 12:53:21 +0200

    docs agregado

---
+++ docs/precache-manifest.0c983dfd5a0a71f1da759ec6fd90cae2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c45bba051c4c0f7e2b51720761a4c89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4304a1fd20609c61162",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "64a359bfa74ba7fc3a1f",
+    "url": "/static/css/main.c1d211bb.chunk.css"
+  },
+  {
+    "revision": "d4304a1fd20609c61162",
+    "url": "/static/js/2.1db0139e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1db0139e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64a359bfa74ba7fc3a1f",
+    "url": "/static/js/main.16856b05.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
